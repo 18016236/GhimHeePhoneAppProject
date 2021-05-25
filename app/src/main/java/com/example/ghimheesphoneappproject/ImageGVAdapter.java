ddfa6117592530575f49dda5dfbd454ec7925cd0
Mervin <18016236@myrp.edu.sg>
2021-05-25 21:05:59 +0800

    6th commit with the completion of the settings page but it does not make the whole screen scroll

--- app/src/main/java/com/example/ghimheesphoneappproject/ImageGVAdapter.java
+++ app/src/main/java/com/example/ghimheesphoneappproject/ImageGVAdapter.java
@@ -1,13 +1,37 @@
 package com.example.ghimheesphoneappproject;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
 
 public class ImageGVAdapter extends ArrayAdapter<OptionsModel> {
 
-    public ImageGVAdapter(@NonNull Context context, int resource, @NonNull OptionsModel[] objects) {
-        super(context, resource, objects);
+    public ImageGVAdapter(@NonNull Context context, ArrayList<OptionsModel> optionsModelArrayList) {
+        super(context, 0, optionsModelArrayList);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View listitemView = convertView;
+        if (listitemView == null) {
+            listitemView = LayoutInflater.from(getContext()).inflate(R.layout.activity_gridview, parent, false);
+        }
+        OptionsModel optionsModel = getItem(position);
+        TextView optionsTV = listitemView.findViewById(R.id.idTVCourse);
+        ImageView optionsIV = listitemView.findViewById(R.id.idOptions);
+        optionsTV.setText(optionsModel.getItem_name());
+        optionsIV.setImageResource(optionsModel.getImgid());
+        return listitemView;
+
     }
 }
--- app/src/main/java/com/example/ghimheesphoneappproject/SettingsFragment.java
+++ app/src/main/java/com/example/ghimheesphoneappproject/SettingsFragment.java
@@ -7,58 +7,32 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SettingsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
+
 public class SettingsFragment extends Fragment {
+    GridView simpleGrid;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public SettingsFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SettingsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SettingsFragment newInstance(String param1, String param2) {
-        SettingsFragment fragment = new SettingsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        simpleGrid = view.findViewById(R.id.GvRecommendations);
+
+        ArrayList<OptionsModel>optionsModelArrayList = new ArrayList<OptionsModel>();
+        optionsModelArrayList.add(new OptionsModel("Malaysian Cabbages",R.drawable.download));
+        optionsModelArrayList.add(new OptionsModel("Dutch Mushrooms",R.drawable.download__1_));
+        optionsModelArrayList.add(new OptionsModel("Thai Broccoli",R.drawable.images));
+        optionsModelArrayList.add(new OptionsModel("Phillipines Cabbages",R.drawable.download2));
+
+        ImageGVAdapter adapter = new ImageGVAdapter(this.getActivity(),optionsModelArrayList);
+        simpleGrid.setAdapter(adapter);
+
+        return view;
     }
 }
